a4aa1d1327a61868f211fc9384fb311ed56a18cb
yosko99 <thedriftmaniaka1997@gmail.com>
2022-05-04 14:11:11 +0300

    Populate city input by allowing location

--- frontend/src/components/inputs/CityInput.tsx
+++ frontend/src/components/inputs/CityInput.tsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import axios from 'axios';
 import { Form } from 'react-bootstrap';
 
 const CityInput = () => {
+  const [currentCity, setCurrentCity] = useState<string>('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((success) => {
+      const { latitude: lat, longitude: lon } = success.coords;
+      axios.post('/api/openweather/city', {
+        lon,
+        lat
+      }).then((response) => {
+        const { city } = response.data;
+
+        setCurrentCity(city);
+      }).catch((err) => {
+        console.log(err.response.data);
+      });
+    });
+  });
+
   return (
 		<Form.Group className="mb-3" controlId="city">
 			<Form.Label>
@@ -13,8 +32,12 @@ const CityInput = () => {
 				className='shadow-sm'
 				pattern='[\w\s]+'
 				required type="text"
+				defaultValue={currentCity}
 				placeholder="New York"
 			/>
+			<Form.Text className="text-muted">
+				By allowing 'current location' we will populate this value for you.
+			</Form.Text>
 		</Form.Group>
   );
 };
